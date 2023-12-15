c3b3e3f9eb3b18edf8e2f07468f9bdd884c844ce
Arsen791 <114273777+Arsen791@users.noreply.github.com>
2023-12-15 09:15:55 +0600

    3

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,50 +1,23 @@
 import React, { Component } from 'react';
 import './header.css';
-import Home from '../home';
-
-
 export default class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentPage: 'home' // Изначально устанавливаем домашнюю страницу
-    };
-  }
-
-  // Функция для перехода на определенную страницу
-  navigateTo = (page) => {
-    this.setState({ currentPage: page }); // Обновляем состояние текущей страницы
-  };
-
-  // Функция для отображения содержимого страницы в зависимости от состояния
-  renderPage = () => {
-    const { currentPage } = this.state;
-    if (currentPage === 'home') {
-      return <Home />;
-    } else {
-
-    }
-    // Другие условия для отображения других компонентов в зависимости от страницы
-    return null; // Здесь можно вернуть другие компоненты для других страниц
-  };
-
   render() {
     return (
       <div>
         <header>
           <div className="logo">
-            <button onClick={() => this.navigateTo('/')}>My Blog</button>
+            <button >My Blog</button>
           </div>
           <nav>
             <ul className="nav-links">
               <li>
-                <button onClick={() => this.navigateTo('home')}>Home</button>
+                <button >Home</button>
               </li>
 
             </ul>
           </nav>
         </header>
-        {this.renderPage()} 
+
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './components/header';
+import App from './components/app';
 
-ReactDOM.render(<Header/>,
+ReactDOM.render(<App/>,
     document.getElementById('root'));
